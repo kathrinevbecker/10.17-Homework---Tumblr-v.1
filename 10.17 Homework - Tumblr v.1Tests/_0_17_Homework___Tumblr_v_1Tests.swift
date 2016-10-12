3ae9e676394d08214551955a8befdff52659ac3c
kathrinevbecker <kathrinevbecker@gmail.com>
2016-10-12 00:58:01 -0700

    Initial Commit

---
+++ 10.17 Homework - Tumblr v.1Tests/_0_17_Homework___Tumblr_v_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _0_17_Homework___Tumblr_v_1Tests.swift
+//  10.17 Homework - Tumblr v.1Tests
+//
+//  Created by Becker, Kathrine V on 10/12/16.
+//  Copyright © 2016 Becker, Kathrine V. All rights reserved.
+//
+
+import XCTest
+@testable import _0_17_Homework___Tumblr_v_1
+
+class _0_17_Homework___Tumblr_v_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
